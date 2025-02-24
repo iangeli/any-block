fd50ac35a087d61c23c1b6afaa54b04e0a234812
Linc <762699299@qq.com>
2025-02-24 15:21:36 +0800

    feat(app): add app version

---
+++ src/App/README.md
@@ -0,0 +1,17 @@
+# AnyBlockApp
+
+App版本的编译：(两种方法都可以)
+
+方法一：
+
+```bash
+cd src/App
+npm install
+npm run dev # or npm run build
+```
+
+方法二：
+
+```bash
+npm run app:dev # or npm run app:build
+```
---
+++ src/App/src/App.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <div>lalala</div>
+  </div>
+</template>
+
+<!--goldenlayout必须样式-->
+<style src="golden-layout/dist/css/goldenlayout-base.css"></style>
+<style src="golden-layout/dist/css/themes/goldenlayout-dark-theme.css"></style>
+
+<style>
+@import "../../ABConverter/style/styles.css";
+
+html, body, #app {
+  height: 100%;
+  border: none;
+  margin: 0;
+  padding: 0;
+  background-color: #313131;
+  color: #c9c9c9;
+}
+</style>
+
+<style lang="scss" scoped>
+.main-nav {
+  height: 28px;
+}
+.main-golden {
+  height: calc(100% - 28px);
+  width: 100%;
+}
+</style>
