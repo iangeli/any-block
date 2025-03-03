89fc4bfa120ba42f58ce4dc62856aa5d3ebc7702
Linc <762699299@qq.com>
2025-03-03 15:15:25 +0800

    fix: wiki link with anchor cause inf refresh2

--- src/ab_manager/abm_html/ABSelector_PostHtml.ts
+++ src/ab_manager/abm_html/ABSelector_PostHtml.ts
@@ -80,10 +80,23 @@ export class ABSelector_PostHtml{
 
         // 判断是否悬浮窗口。悬浮窗口禁用强制渲染
         const view: View|null = app.workspace.getActiveViewOfType(MarkdownView); // 未聚焦(active)会返回null
+        // 判断方式一：内容与窗口的文件名是否一致 (切换页面时 (aIncludeB -> b)，有可能检测有问题，要用另一判断方式)
         // @ts-ignore 类型“View”上不存在属性“file”
         const path = view?.file.path
         if (path && path !== ctx.sourcePath) {
           if (this.settings.is_debug) console.log(` !! Cache check: [${path}] use ![[${ctx.sourcePath}]] `)
+          cache_item = { // 注意，极难检测是否 `[[#]]`，不存cache_map，也不触发强制刷新
+            name: ctx.sourcePath,
+            content: mdSrc.content_all
+          }
+          is_newContent = false
+          is_subContent = true
+          return
+        }
+        // 判断方式二：是否是实时模式下显示阅读模式内容
+        const el = view?.containerEl // .workspace-leaf-content
+        if (el && el.getAttribute('data-mode') != 'preview') { // source
+          if (this.settings.is_debug) console.log(` !! Cache check: [${path}] use ![[${ctx.sourcePath}]] in source Mode`)
           cache_item = { // 注意，极难检测是否 `[[#]]`，不存cache_map，也不触发强制刷新
             name: ctx.sourcePath,
             content: mdSrc.content_all
