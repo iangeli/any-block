1bab9ab004691cef75eaaec94326feb3ea432a75
Linc <762699299@qq.com>
2025-05-12 16:48:39 +0800

    fix: list2dt/dir, folder shape noignore some time

--- src/ABConverter/converter/abc_dir_tree.ts
+++ src/ABConverter/converter/abc_dir_tree.ts
@@ -110,7 +110,7 @@ export class DirProcess{
         }
         // 填充
         list_itemInfo2.push({
-          content: list_column_item[column_index], // modi
+          content: list_column_item[column_index].trimEnd(), // modi, 使用内联分割符时，尾空白应该总是无用的
           level: row_item.level,
           tableRow: row_index, // modi
           tableColumn: column_index, // modi
@@ -194,7 +194,7 @@ export class DirProcess{
           if (!is_head) {
             if (cell_item.type=="folder") {
               td_svg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z"/></svg>`
-              cell_item.content = cell_item.content.slice(0, -1);
+              cell_item.content = cell_item.content.trimEnd().slice(0, -1);
             } else {
               td_svg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M320 464c8.8 0 16-7.2 16-16l0-288-80 0c-17.7 0-32-14.3-32-32l0-80L64 48c-8.8 0-16 7.2-16 16l0 384c0 8.8 7.2 16 16 16l256 0zM0 64C0 28.7 28.7 0 64 0L229.5 0c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3L384 448c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64z"/></svg>`
             }
@@ -421,7 +421,7 @@ function listdata2dirdata(list: List_ListItem): List_DirListItem {
 
     // 文件扩展名
     let type: string;
-    if (item.content.endsWith("/")) {
+    if (item.content.trimEnd().endsWith("/")) {
       type = "folder"
     } else {
       const parts = item.content.split('.');
