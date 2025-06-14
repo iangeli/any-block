28221b06e1fc26898969df5c864fbe8a8b032983
Linc <762699299@qq.com>
2025-06-14 16:33:26 +0800

    enhance: use sentence case in UI

--- src/Obsidian/config/ABSettingTab.ts
+++ src/Obsidian/config/ABSettingTab.ts
@@ -140,7 +140,7 @@ export class ABSettingTab extends PluginSettingTab {
     containerEl.createEl('hr', {cls: "bright-color"})
 
     // 选择器管理
-    new Setting(containerEl).setName('Selector Manager (选择器的管理)').setHeading();
+    new Setting(containerEl).setName('Selector manager (选择器的管理)').setHeading();
     containerEl.createEl('p', {text: 'This section is for query only and cannot be edited (这一部分仅供查询不可编辑)'})
     this.selectorPanel = generateSelectorInfoTable(containerEl)
     containerEl.createEl('hr', {cls: "bright-color"})
@@ -193,7 +193,7 @@ export class ABSettingTab extends PluginSettingTab {
       })*/
 
     // 别名系统的管理
-    new Setting(containerEl).setName('AliasSystem Manager (别名系统的管理)').setHeading();
+    new Setting(containerEl).setName('AliasSystem manager (别名系统的管理)').setHeading();
     containerEl.createEl('p', {text: 'It can also be viewed in the main page using the `[info_alias]` processor (这部分内容也可以使用 `[info_alias]` 处理器在主页面中查看)'});
     containerEl.createEl('p', {text: 'This section can also be modified by opening the `data.json` file in the plugin folder (这部分也可以打开插件文件夹中的 `data.json` 文件修改)'});
     new Setting(containerEl)
@@ -244,7 +244,7 @@ export class ABSettingTab extends PluginSettingTab {
       containerEl.createEl('hr', {cls: "bright-color"})
 
     // 转换器的管理
-    new Setting(containerEl).setName('Convertor Manager (转换器的管理)').setHeading();
+    new Setting(containerEl).setName('Convertor manager (转换器的管理)').setHeading();
     containerEl.createEl('p', {text: 'It can also be viewed in the main page using the `[info_converter]` processor (这部分内容也可以使用 `[info_converter]` 处理器在主页面中查看)'});
     containerEl.createEl('p', {text: 'This section is for query only and cannot be edited (这一部分仅供查询不可编辑)'})
     containerEl.createEl('p', {text: ''});
@@ -277,7 +277,7 @@ class ABProcessorModal extends Modal {
     contentEl.setText("Custom processor (自定义处理器)");
     contentEl.createEl("p", {text: ""})
     new Setting(contentEl)
-      .setName("ProcessorId")
+      .setName("Processor id")
       .setDesc("处理器唯一id, 不与其他处理器冲突即可")
       .addText((text)=>{
         text.onChange((value) => {
@@ -286,7 +286,7 @@ class ABProcessorModal extends Modal {
       })
 
     new Setting(contentEl)
-      .setName("ProcessorName")
+      .setName("Processor name")
       .setDesc("注册器名，可以乱填，给自己看的")
       .addText((text)=>{
         text.onChange((value) => {
