8f1e8aa5a9968a29f1f04de13fe77767a06a0c5f
Hares <harryzjm@live.com>
2025-06-17 14:10:11 +0800

    remove unused

--- src/ABConverter/index.ts
+++ src/ABConverter/index.ts
@@ -5,9 +5,6 @@
 
 // 转换器模块
 export { ABConvertManager } from "./ABConvertManager"
-// 加载所有转换器 (都是可选的，可自行增删。当然，如果A转换器依赖B转换器，那么你导入A必然导入B)
-export {} from "./converter/abc_text"
-export {} from "./converter/abc_code"
 export {} from "./converter/abc_list"
 export {} from "./converter/abc_c2list"
 export {} from "./converter/abc_table"
@@ -15,9 +12,6 @@ export {} from "./converter/abc_dir_tree"
 export {} from "./converter/abc_deco"
 export {} from "./converter/abc_ex"
 export {} from "./converter/abc_mdit_container"
-export {} from "./converter/abc_plantuml" // 可选建议：156.3KB。由于在线渲染，相对下面两个没那么高
-export {} from "./converter/abc_mermaid"  // 可选建议：新版无额外依赖，旧版非 min 环境下 7.1MB
-export {} from "./converter/abc_markmap"  // 可选建议：1.3MB
 
 // 定义环境条件
 import { ABCSetting } from "./ABReg"
